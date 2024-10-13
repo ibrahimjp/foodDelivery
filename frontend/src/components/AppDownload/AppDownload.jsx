ef34166deeb0c007f801b7e32230eb7e7f56c2f7
aayushai <aayushhp17@gmail.com>
2024-10-14 03:59:49 +0530

    Fix: app download component

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import './AppDownload.css'
-import { assets } from '../../assets/assets'
+import React from "react";
+import "./AppDownload.css";
+import { assets } from "../../assets/assets";
 
 const AppDownload = () => {
-  return (
-    <div className='app-download' id='app-download'>
-        <p className='pforbetter'>For Better Experience Download <br /> Hangry App</p>
-        <div className="app-download-platforms">
-            <img src={assets.play_store} alt="" />
-            <img src={assets.app_store} alt="" />
+    return (
+        <div className="app-download" id="app-download">
+            <p className="pforbetter">
+                For Better Experience Download <br /> Hangry App
+            </p>
+            <div className="app-download-platforms">
+                <img src={assets.play_store} alt="" />
+                <img src={assets.app_store} alt="" />
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default AppDownload
+export default AppDownload;
