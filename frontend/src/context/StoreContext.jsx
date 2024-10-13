16e05cca430d0ff7f489292bbe97ad154349eddf
aayushai <aayushhp17@gmail.com>
2024-10-14 04:07:53 +0530

    Fix: hide backend url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "https://hangry-backend.onrender.com"
+    const url = process.env.API_URL;
     const [token,setToken] = useState("")
     const [food_list,setFoodList] = useState([])
 
